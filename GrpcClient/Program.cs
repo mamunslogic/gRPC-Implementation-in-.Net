4d60d6463bc21d23444aec94c26e1141013b894c
Mamun <mamunslogic@hotmail.com>
2024-05-30 02:35:20 +0600

    gRPC Server and Client both are uploaded

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using Grpc.Net.Client;
+using GrpcServer.Protos;
+
+GrpcChannel channel = GrpcChannel.ForAddress("https://localhost:7068");
+Tweeter.TweeterClient client = new(channel);
+
+var request = new TweetRequest { Message = "Hi" };
+var reply = await client.SendTweetAsync(request);
+
+Console.WriteLine($"Request from client: {request.Message}");
+Console.WriteLine($"Response from server: {reply.Message}");
+Console.ReadLine();
---
+++ GrpcServer/Services/TweeterService.cs
@@ -0,0 +1,13 @@
+﻿using Grpc.Core;
+using GrpcServer.Protos;
+
+namespace GrpcServer.Services
+{
+    public class TweeterService : Tweeter.TweeterBase
+    {
+        public override Task<TweetReply> SendTweet(TweetRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new TweetReply { Message= "Hello. I think you are doing well."});
+        }
+    }
+}
